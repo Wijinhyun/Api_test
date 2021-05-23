619564cb5fc111d3707d3cd5fda42e3ee9b26b05
NaJeongHu <65152311+NaJeongHu@users.noreply.github.com>
2021-05-23 16:48:53 +0900

    renewal splash

--- app/src/main/java/com/prodoc/api_test/StartActivity.java
+++ app/src/main/java/com/prodoc/api_test/StartActivity.java
@@ -1,7 +1,9 @@
 package com.prodoc.api_test;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
@@ -10,21 +12,55 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class StartActivity extends AppCompatActivity {
 
     TextView t1;
     LinearLayout linear;
+    LottieAnimationView animationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        t1 = findViewById(R.id.start_text);
-        linear = findViewById(R.id.start_linear);
-        Animation c = AnimationUtils.loadAnimation(StartActivity.this,R.anim.alphaex);
-        linear.startAnimation(c);
+//        t1 = findViewById(R.id.start_text);
+//        linear = findViewById(R.id.start_linear);
+//        Animation c = AnimationUtils.loadAnimation(StartActivity.this,R.anim.alphaex);
+//        linear.startAnimation(c);
 
+        animationView = (LottieAnimationView) findViewById(R.id.start_lottie);
+        animationView.playAnimation();
+
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                animationView.setVisibility(View.INVISIBLE);
+                Intent intent1 = new Intent(getApplicationContext(), MainActivity_Ver2.class);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent1);
+                finish();
+                overridePendingTransition(0,0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        /*
         c.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -45,7 +81,7 @@ public class StartActivity extends AppCompatActivity {
 
             }
         });
-
+*/
 
 
     }
